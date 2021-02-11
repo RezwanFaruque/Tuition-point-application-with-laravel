9a517de47e838c22194a1f6ea5bc1bbd88ebaf40
RezwanFaruque <rezwanfaruque9@gmail.com>
2021-02-12 00:39:25 +0600

    auth scrapfolding normal added adjust required

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('phone_number')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('is_tutor')->nullable()->default('0');
+            $table->string('is_gurdian')->nullable()->default('0');
+            $table->string('is_admin')->nullable()->default('0');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
